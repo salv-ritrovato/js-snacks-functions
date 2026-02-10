b32a72557776715b2219b7ea67e16c2e60aa2f7e
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-10 13:02:31 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+const userName = 'Mario';
+
+// Dichiara la funzione qui.
+function waveAt() {
+    const salute = 'Ciao' + ' ' + userName
+    return salute
+}
+// Invoca la funzione qui e stampa il risultato in console
+// Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(waveAt());
+
+// ARROW FUNCTION 
+const hello = userName => 'Ciao ' + userName;
+console.log(hello("Mario"));
