d804944c0e75ddf478a231ae84603ae0924d3e65
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-10 15:25:28 +0100

    task 3 and 4 completed

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,30 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 const myLetters = [];
-
-
 // Dichiara la funzione qui.
-
-
+function sameLetters() {
+    for (let i = 0; i < names.length; i++) {
+        if (names[i].charAt(0) === 'A') {
+            myLetters.push(names[i])
+        }
+}
+return myLetters
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log(sameLetters());
+
+// ARROW FUNCTION 
+const myLettersA = [];
+const letters = () => { 
+    for (let i = 0; i < names.length; i++) {
+        if (names[i].charAt(0) === 'A') {
+            myLetters.push(names[i])
+        }
+}
+return myLetters
+}
+
+console.log(letters());
+
+
--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
-
 const word = 'javascript';
+const vowelsHolder = [];
 
-
-// Dichiara la funzione qui.
-
+// Dichiara la funzione qui
+function vowels() {
+    for (let i = 0; i < word.length; i++)
+        if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+            vowelsHolder.push(word[i])
+        }
+    return vowelsHolder.length
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(vowels());
