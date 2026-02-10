54f035646eccb1b88d6d4422d06515203357c67c
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-10 15:33:01 +0100

    task 4 - added new method and arrow function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -14,3 +14,35 @@ function vowels() {
 // Invoca la funzione qui e stampa il risultato in console
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
 console.log(vowels());
+
+// ARROW FUNCTION 
+vowelsHolderA = [];
+const vowelS = () => {
+    for (let i = 0; i < word.length; i++)
+        if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+            vowelsHolderA.push(word[i])
+        }
+    return vowelsHolderA.length
+}
+console.log(vowelS());
+
+
+
+
+
+// METODO ALTERNATIVO
+/* function count() {
+    const vocali = ['a', 'e', 'i', 'o', 'u'];
+    let contatore = 0;
+    for (let i = 0; i < word.length; i++) {
+         let char = word[i];
+    for (let j = 0; j < vocali.length; j++) {
+    if (char === vocali[j]) {
+        contatore++;
+        }
+    }
+}
+return contatore;
+}
+
+console.log(count()); */
