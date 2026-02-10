e46f29e15942edb027ce0a53a419be44ea0b03aa
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-10 15:43:47 +0100

    working on bonus obj, doesn't work yet

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -4,15 +4,20 @@ buongiorno se è mattina (fino alle 13),
 buon pomeriggio se è pomeriggio (fino alle 17) 
 e buonasera se è sera (oltre le 17)
 */
-
 const name = 'Mario';
 
-
 // Dichiara la funzione qui.
-
+function waveHour() {
+    const timeDate = new Date().getHours();
+    if (timeDate <= 13) {
+        return 'Buongiorno' + ' ' + name;
+    } else if (timeDate <= 17) {
+        return "Buon Pomeriggio" + ' ' + name;
+    } else {
+        return "Buonasera" + ' ' + name;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
-//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+// Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario
+console.log(waveHour(name));
